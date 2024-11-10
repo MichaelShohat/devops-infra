af7f6a5cd882b9ccc868de0de84e1d5543507439
Michael Shohat <michaelsho9@gmail.com>
2024-11-10 15:00:29 +0200

    trying to debug permission error

---
+++ main.tf
@@ -0,0 +1,48 @@
+provider "aws" {
+  region = local.region
+}
+
+data "aws_availability_zones" "available" {
+    state = "available"
+}
+
+locals {
+  name   = "eks-michaels"
+  region = "eu-west-1"
+
+  vpc_cidr = "10.0.0.0/16"
+  azs      = slice(data.aws_availability_zones.available.names, 0, 2)
+  
+  tags = {
+    Desc = "provisioned my Michael Shohat for home assigment"
+  }
+}
+
+################################################################################
+# VPC
+################################################################################
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = local.name
+  cidr = local.vpc_cidr
+
+  azs             = local.azs
+  private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k)]
+  public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k + 2)]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
+
+  tags = local.tags
+}
